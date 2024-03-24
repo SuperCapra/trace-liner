87dfbfd52e952f69ee26ba82738953a34055f8be
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-03-24 21:00:03 +0100

    added .env file to git ignore and added the client id

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+const clientId = process.env.REACT_APP_STRAVA_CLIENT_ID
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Homepage/>
   );
 }
+
+class Homepage extends React.Component{
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = {
+  //   //   stage : stage,
+  //   //   stageHistory : stageHistory
+  //   // }
+  // }
+  
+  // routesToStravaLogin() {
+  //   if(!Boolean(active)) {
+  //     return (
+  //       <code className="code-inactive">{message}</code>
+  //     )
+  //   }
+  //   if(this.state.stage === 'LanguageSelection') {
+  //     return (<LanguageSelection onClick={value => this.changeStage(value)}/>)
+  //   } else if(this.state.stage === 'FirstStep') {
+  //     return (<FirstStep onInteractiveSelection={value => this.changeStage(value)}/>)
+  //   } else if(this.state.stage === 'Interactive') return (
+  //     <Interactive/>
+  //   )
+  // }
+
+  // returnRadioLang() {
+  //   if(language) return (
+  //     <RadioLang className="radio-lang" onChangeLanguage={value => this.changeStage(value)} onBack={() => this.goBack()}/>
+  //   )
+  //   return (<div></div>)
+  // }
+
+  // goBack() {
+  //   console.log('stageHistory', stageHistory)
+  //   stageHistory.pop()
+  //   this.changeStage({stage: stageHistory[stageHistory.length - 1]})
+  //   console.log('stageHistory', stageHistory)
+  // }
+
+  // returnBack() {
+  //   if(stageHistory[stageHistory.length - 1] === 'Interactive') {
+  //     return (<Back onBack={() => this.goBack()}/>)
+  //   } else {
+  //     return (<div></div>)
+  //   }
+  // }
+
+  render() {
+    return (    
+      <div className="App">
+          {/* {this.returnRadioLang()}
+          {this.returnBack()} */}
+        <div className="App-header">
+          <div>
+            {/* {this.routesToStage()} */}
+            ciao!
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+// class LanguageSelection extends React.Component {
+//   cycleLanguages() {
+//     return(
+//       <div>
+//         {languages.map(x => {
+//           return(
+//             <div key={x.value} onClick={() => this.props.onClick({stage: 'FirstStep', language: x.value})} className="div-cliccable">
+//               <p name={x.value}><code>{x.label}</code></p>
+//             </div>
+//           )
+//         })}
+//       </div>
+//     )
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         {this.cycleLanguages()}
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
