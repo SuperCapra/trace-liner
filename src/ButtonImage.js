66b774ea3e464e77f3ea71e4bb95dc3abc67e177
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-03-29 17:07:49 +0100

    redefined the scale of the icons in the right place

--- src/ButtonImage.js
+++ src/ButtonImage.js
@@ -8,18 +8,19 @@ class ButtonImage extends React.Component{
     render() {
         const shareStyle = {
             fill: brandingPalette.pink,
-            margin: '1vw 2vw 1vw 2vw',
+            // margin: '1vw 2vw 1vw 2vw',
+            transform: 'scale(0.5)'
         }
         const modifyStyle = {
             fill: brandingPalette.pink,
-            margin: '1vw 2vw 1vw 2vw'
+            // margin: '1vw 2vw 1vw 2vw',
+            transform: 'scale(0.5)'
         }
         return(
             <div className="wrapper-buttons">
                 <div style={modifyStyle} onClick={() => this.props.clickModify()}>
                     <ModifySVG />
                 </div>
-
                 <div style={shareStyle} onClick={() => this.props.clickShare()}>
                     <ShareSVG />
                 </div>
