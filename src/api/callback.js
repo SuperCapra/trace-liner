4b497d3bae22702151d39cf5a38a23c93ed547d0
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-04-20 16:52:53 +0200

    added webhook

--- src/api/callback.js
+++ src/api/callback.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // Example endpoint
-app.get('/api/data', (req, res) => {
-    res.json({ message: "Hello from the server!" });
-});
+app.get('/api/strava_webhook', (req, res) => {
+    res.json({ 'hub.mode': 'subscribe','hub.challenge': 'oN6G8xMkPNrJtTd3PRohCLXjJLHxLpymJJwWpDza','hub.verify_token': 'STRAVA_BEAUTYLINER'});
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
