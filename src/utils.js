4aa6f9c3471c4f2dee1aefeee5d48f150fba319a
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-06-21 15:48:43 +0200

    added remove emoji utils and used the toJpeg to handle the blending

--- src/utils.js
+++ src/utils.js
@@ -193,7 +193,20 @@ const utilsFunction = {
     },
 
     removeEmoji(text) {
-        return text.replaceAll('[^\\p{L}\\p{M}\\p{N}\\p{P}\\p{Z}\\p{Cf}\\p{Cs}\\s]','')
+        return text.replace(/[\u{1F600}-\u{1F64F}]/gu, '')  // Emoticons
+            .replace(/[\u{1F300}-\u{1F5FF}]/gu, '')  // Symbols & Pictographs
+            .replace(/[\u{1F680}-\u{1F6FF}]/gu, '')  // Transport & Map Symbols
+            .replace(/[\u{1F700}-\u{1F77F}]/gu, '')  // Alchemical Symbols
+            .replace(/[\u{1F780}-\u{1F7FF}]/gu, '')  // Geometric Shapes Extended
+            .replace(/[\u{1F800}-\u{1F8FF}]/gu, '')  // Supplemental Arrows-C
+            .replace(/[\u{1F900}-\u{1F9FF}]/gu, '')  // Supplemental Symbols and Pictographs
+            .replace(/[\u{1FA00}-\u{1FA6F}]/gu, '')  // Chess Symbols
+            .replace(/[\u{1FA70}-\u{1FAFF}]/gu, '')  // Symbols and Pictographs Extended-A
+            .replace(/[\u{2600}-\u{26FF}]/gu, '')    // Miscellaneous Symbols
+            .replace(/[\u{2700}-\u{27BF}]/gu, '')    // Dingbats
+            .replace(/[\u{FE00}-\u{FE0F}]/gu, '')    // Variation Selectors
+            .replace(/[\u{1F1E6}-\u{1F1FF}]/gu, '')
+            .trim(); //
     },
 
     getAverageSpeedMetric(distance, duration) {
