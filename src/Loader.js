2c10eb3cfacbcdbf0e1077418127db385d23ae2d
Giovanni Maggi <gvnnmaggi@gmail.com>
2024-11-02 19:19:39 +0100

    added overlay, set right color for loader and other little tweaks

--- src/Loader.js
+++ src/Loader.js
@@ -1,12 +1,16 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import brandingPalette from './brandingPalette';
 
-class Loader extends React.Component{
-    render() {
-        return(
-            <div className="dot-typing"></div>
-        )
-    }
+function Loader(props) {
+    const {color} = props
+    useEffect(() => {
+        if(brandingPalette[color]) document.documentElement.style.setProperty('--dot-typing-color', brandingPalette[color])
+        else document.documentElement.style.setProperty('--dot-typing-color', brandingPalette.primary)
+      }, [color]);
+    return(
+        <div className="dot-typing"></div>
+    )
 }
 
 export default Loader;
