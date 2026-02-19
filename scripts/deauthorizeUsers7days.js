44c06fbf6047a7e293e0b6fb1cbaea26712f2549
Giovanni Maggi <gvnnmaggi@gmail.com>
2026-02-19 13:51:10 +0100

    bug fix

--- scripts/deauthorizeUsers7days.js
+++ scripts/deauthorizeUsers7days.js
@@ -18,20 +18,20 @@ async function run() {
     //console.log('ros[0]', users.rows[0])
     console.log(`Found ${users.rows.length} users to deauthorize.`)
     for (const u of users.rows) {
-        if(u.access_token === null || u.refresh_token === null) {
-            console.error(`Skipping user ${u.user_id} due to null tokens.`)
-            continue
-        }
-        if(u.refresh_token.length > 40) {
-            const res = (`
-                SELECT user_id, pgp_sym_decrypt(pgp_sym_decrypt(auth_token, $1)::bytea, $1) AS access_token, pgp_sym_decrypt(pgp_sym_decrypt(refresh_token, $1)::bytea, $1) AS refresh_token, users.Name, users.lastmodified_at
-                FROM users_auth
-                INNER JOIN users ON users.id = users_auth.user_id
-                WHERE users.lastmodified_at < NOW() - INTERVAL '7 days' and users_auth.user_id = $2
-            `, [process.env.TOKEN_ENCRYPTION_KEY, u.user_id])
-            u = res.rows[0]
-        }
         try {
+            if(u.access_token === null || u.refresh_token === null) {
+                console.error(`Skipping user ${u.user_id} due to null tokens.`)
+                continue
+            }
+            if(u.refresh_token.length > 40) {
+                const res = (`
+                    SELECT user_id, pgp_sym_decrypt(pgp_sym_decrypt(auth_token, $1)::bytea, $1) AS access_token, pgp_sym_decrypt(pgp_sym_decrypt(refresh_token, $1)::bytea, $1) AS refresh_token, users.Name, users.lastmodified_at
+                    FROM users_auth
+                    INNER JOIN users ON users.id = users_auth.user_id
+                    WHERE users.lastmodified_at < NOW() - INTERVAL '7 days' and users_auth.user_id = $2
+                `, [process.env.TOKEN_ENCRYPTION_KEY, u.user_id])
+                u = res.rows[0]
+            }
             let response = await deauthorizeStrava(u.access_token)
             console.log(`Deauthorized user ${u.user_id}`)
             if(response.errors) {
