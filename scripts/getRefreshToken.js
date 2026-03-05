7a5c21c9ed6739138442111956fc75ebf30fbf91
Giovanni Maggi <gvnnmaggi@gmail.com>
2026-03-05 08:15:35 +0100

    included not death user 1 and removed some consol.log

--- scripts/getRefreshToken.js
+++ scripts/getRefreshToken.js
@@ -8,8 +8,8 @@ async function run() {
     const users = await pool.query(`
         SELECT user_id, pgp_sym_decrypt(auth_token, $1) AS access_token, pgp_sym_decrypt(refresh_token, $1) AS refresh_token, users.Name, users.lastmodified_at
         FROM users_auth
-        INNER JOIN users ON users.id = users_auth.user_id  AND users.id != '1'
-        WHERE users.lastmodified_at < NOW()
+        INNER JOIN users ON users.id = users_auth.user_id
+        WHERE users.id != '1'
     `, [process.env.TOKEN_ENCRYPTION_KEY])
     if(!users || !users.rows) {
         console.log('No users found to deauthorize.')
@@ -19,6 +19,7 @@ async function run() {
     console.log(`Found ${users.rows.length} users to deauthorize.`)
     for (const u of users.rows) {
         try {
+            console.log('u', u)
             if(u.access_token === null || u.refresh_token === null) {
                 console.error(`Skipping user ${u.user_id} due to null tokens.`)
                 continue
@@ -32,26 +33,7 @@ async function run() {
                 `, [process.env.TOKEN_ENCRYPTION_KEY, u.user_id])
                 u = res.rows[0]
             }
-            let response = await deauthorizeStrava(u.access_token)
-            console.log(`Deauthorized user ${u.user_id}`)
-            if(response.errors) {
-                let responseRefresh = await refreshToken(u.refresh_token)
-                if(!responseRefresh.errors) {
-                    let newAccessToken = responseRefresh.access_token
-                    let responseDeauth = await deauthorizeStrava(newAccessToken)
-                    await pool.query(`
-                        DELETE FROM users_auth
-                        WHERE user_id = $1
-                    `, [u.user_id])
-                } else {
-                    console.error(`Could not refresh token for user ${u.user_id}, skipping deauthorization.`)
-                }
-            } else {
-                await pool.query(`
-                    DELETE FROM users_auth
-                    WHERE user_id = $1
-                `, [u.user_id])
-            }
+            console.log('u', u.refresh_token)
         } catch (e) {
             console.error(`Error deauthorizing user ${u.user_id}:`, e)
         }
